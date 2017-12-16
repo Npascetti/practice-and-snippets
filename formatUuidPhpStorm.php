2d0714404058bbd440707accff4cc13618d9b404
Npascetti <31805340+Npascetti@users.noreply.github.com>
2017-12-16 00:56:01 -0700

    function that formats uuids from mySQL database

---
+++ formatUuidPhpStorm.php
@@ -0,0 +1,14 @@
+<!php
+///////input stringy uuid as argument with formatting directly out of PhpStorm mySQL database including leading 0x //////////////
+///////this will output a uuid string in a format that can be passed into the url, and is useful for testing RESTful services////
+
+function formatuuid($uuidstring) {
+    $uuidpattern = "/(\w{2})(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/i";
+    $uuidreplace = "$2-$3-$4-$5-$6";
+    $formatteduuid =  strtolower(preg_replace($uuidpattern, $uuidreplace, $uuidstring));
+    echo $formatteduuid;
+    
+}
+
+// example //////formatuuid("0xD57FE8FB3C2F4D75AB60AFF88C61CE6B");//////
+/////this example will output "d57fe8fb-3c2f-4d75-ab60-aff88c61ce6b"////
