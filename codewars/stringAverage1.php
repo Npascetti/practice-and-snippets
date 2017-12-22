4422b79a3daf2fd346c0cbd66f5152e998b3d7ed
Nick Pascetti <npascetti@cnm.edu>
2017-12-21 20:46:03 -0700

    added comments to better stringAverage function

--- codewars/stringAverage1.php
+++ codewars/stringAverage1.php
@@ -5,9 +5,11 @@
 **/
 function average_string(string $s): string
 {
+  ///if block returning "n/a" if input is an empty string///
     if ($s === '') {
         return 'n/a';
     }
+    ///associative array containing index of stringy numbers and values as ints///
     $values = [
         'zero'  => 0,
         'one'   => 1,
@@ -20,15 +22,21 @@ function average_string(string $s): string
         'eight' => 8,
         'nine'  => 9,
     ];
+    ///exploding input into array, initializing $sum///
 
     $words = explode(' ', $s);
     $sum = 0;
+    ///foreach loop taking the values of $words and setting them to index of $values
     foreach ($words as $word) {
+      ///returns "n/a" if the input value is not valid///
         if (!isset($values[$word])) {
             return 'n/a';
         }
+        ///summing up the int values corresponding to the index from $words in the $values array///
         $sum += $values[$word];
     }
+    ///averages the sum to the floor///
     $avg = floor($sum / count($words));
+    ///returns stringy index from $values array based on the value of $avg///
     return array_search($avg, $values);
 }
