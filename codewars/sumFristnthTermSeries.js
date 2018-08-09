d26cc8721416d8c69655b6a20b0a4815491f9414
Nick Pascetti <Npascetti@gmail.com>
2018-08-08 21:15:19 -0600

    started refactor to js

--- codewars/sumFristnthTermSeries.js
+++ codewars/sumFristnthTermSeries.js
@@ -1,9 +1,10 @@
 function series_sum(n) {
-  if n == 0 or n == 1:
-      return str(n) + ".00"
-  denominator = 4
-  algo = 1
-  for i in range(n - 1):
+  if (var n == 0 or n == 1) {
+    return str(n) + ".00"
+  }
+  var denominator = 4
+  var algo = 1
+  for var i in range(n - 1):
       algo += 1/denominator
       denominator += 3
   if len(str(round(algo, 2))) == 3:
