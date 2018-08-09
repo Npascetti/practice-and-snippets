68357645c0b183a270763e29c19f59e4b835cd39
Nick Pascetti <Npascetti@gmail.com>
2018-08-08 19:28:12 -0600

    commiting wsumFirstnthTerm

---
+++ codewars/sumFirstnthTermSeries.py
@@ -0,0 +1,11 @@
+def series_sum(n):
+    if n == 0 or n == 1:
+        return str(n) + ".00"
+    denominator = 4
+    algo = 1
+    for i in range(n - 1):
+        algo += 1/denominator
+        denominator += 3
+    if len(str(round(algo, 2))) == 3:
+        return str(round(algo, 2)) + "0"
+    return str(round(algo, 2))
