bbcab8db4b228a358fe4be00d00616d084540bdf
Nick Pascetti <Npascetti@gmail.com>
2018-08-09 20:25:39 -0600

    merging

--- codewars/sumFristnthTermSeries.js
+++ codewars/sumFristnthTermSeries.js
@@ -1,14 +1,12 @@
-function series_sum(n) {
-  if (var n == 0 or n == 1) {
-    return str(n) + ".00"
+function SeriesSum(n) {
+  if (n == 0) {
+    return n.toFixed(2)
   }
   var denominator = 4
   var algo = 1
-  for (var i in range(n - 1)){
+  for (var i = 1; i < n; i++) {
     algo += 1/denominator
     denominator += 3
-if len(str(round(algo, 2))) == 3:
-    return str(round(algo, 2)) + "0"
-return str(round(algo, 2))
   }
+  return algo.toFixed(2)
 }
