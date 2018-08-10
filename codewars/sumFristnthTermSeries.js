f249e0f064fa7d2e703c111744bc89d1994afac7
Nick Pascetti <Npascetti@gmail.com>
2018-08-09 20:27:36 -0600

    merging

--- codewars/sumFristnthTermSeries.js
+++ codewars/sumFristnthTermSeries.js
@@ -4,9 +4,19 @@ function SeriesSum(n) {
   }
   var denominator = 4
   var algo = 1
+<<<<<<< HEAD
   for (var i = 1; i < n; i++) {
     algo += 1/denominator
     denominator += 3
   }
   return algo.toFixed(2)
+=======
+  for (var i in range(n - 1)){
+    algo += 1/denominator
+    denominator += 3
+if len(str(round(algo, 2))) == 3:
+    return str(round(algo, 2)) + "0"
+return str(round(algo, 2))
+  }
+>>>>>>> added some curly braces...
 }
