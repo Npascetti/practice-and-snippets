64c61923e55e6564dee3a3579ab1921e0913c724
Nick Pascetti <Npascetti@gmail.com>
2018-08-12 23:20:29 -0600

    wrote persistentBugger function

---
+++ codewars/persistentBugger.js
@@ -0,0 +1,14 @@
+function persistence(num) {
+    num = ""+num
+    var iterCounter = 0
+    numArray = num.split("")
+    while (numArray.length > 1) {
+        var counter = 1
+        numArray.forEach(function(digit) {
+            counter *= parseInt(digit)
+        })
+        iterCounter += 1
+        numArray = counter.toString(10).split("")
+    }
+    return iterCounter
+}
