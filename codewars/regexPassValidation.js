c119eb2c59ec6204a708481d1d8627cc84d2e829
Nick Pascetti <Npascetti@gmail.com>
2018-08-30 20:33:12 -0600

    added comment for explanation

--- codewars/regexPassValidation.js
+++ codewars/regexPassValidation.js
@@ -1,4 +1,4 @@
-validates a password that is at least 6 characters long and contains least one cap, lowercase and number ONLY - that is no special characters
+// validates a password that is at least 6 characters long and contains least one cap, lowercase and number ONLY - that is no special characters//
 function validate(password) {
   return /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])([a-zA-Z0-9]{6,})$/.test(password);
   }
